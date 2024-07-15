7a87f446030a414b726d5e872d301f52e2595fc0
himanshu <himanshu@doodle-blue.com>
2024-07-15 20:11:26 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { fetchArticles } from './services/api';
+import ArticlesList from './components/ArticleList';
+import ArticleDetailWrapper from './components/ArticleDetailWrapper';
 
-function App() {
+
+const App = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const getArticles = async () => {
+      const articles = await fetchArticles();
+      setArticles(articles);
+    };
+    getArticles();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<ArticlesList articles={articles} />} />
+        <Route path="/article/:id" element={<ArticleDetailWrapper articles={articles} />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
